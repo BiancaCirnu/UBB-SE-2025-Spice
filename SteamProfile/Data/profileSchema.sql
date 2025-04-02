878db0255126eae2f318175be7901a11d0f9bc3e
Alexia-Ioana Bortos <alexiabortos@gmail.com>
2025-04-03 01:49:27 +0300

    updated schema

--- SteamProfile/Data/profileSchema.sql
+++ SteamProfile/Data/profileSchema.sql
@@ -465,6 +465,7 @@ CREATE TABLE UserProfiles (
     profile_id INT IDENTITY(1,1) PRIMARY KEY,
     user_id INT NOT NULL UNIQUE,
     profile_picture NVARCHAR(255) CHECK (profile_picture LIKE '%.svg' OR profile_picture LIKE '%.png' OR profile_picture LIKE '%.jpg'),
+    bio NVARCHAR(1000),
     last_modified DATETIME NOT NULL DEFAULT GETDATE(),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
@@ -539,6 +540,7 @@ END;
 
 
 ----------------------------- PASSWORD RESET CODES --------------------------------
+go
 CREATE TABLE PasswordResetCodes (
     id INT IDENTITY(1,1) PRIMARY KEY,
     user_id INT NOT NULL,
